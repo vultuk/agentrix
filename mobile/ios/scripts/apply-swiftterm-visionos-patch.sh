6cd45ac390d086278b8ab47b15707cb963d05a68
Simon Skinner <talkto@simonskinner.me>
2025-11-14 19:41:21 +0000

    Updated

--- mobile/ios/scripts/apply-swiftterm-visionos-patch.sh
+++ mobile/ios/scripts/apply-swiftterm-visionos-patch.sh
@@ -4,4 +4,5 @@ SP_DIR="${SOURCEPACKAGES_DIR_PATH:-}"
 if [[ -z "$SP_DIR" ]]; then
   exit 0
 fi
-python3 /var/tmp/apply-swiftterm-visionos-patch.py "$SP_DIR"
+
+python3 "${PROJECT_DIR}/scripts/apply-swiftterm-visionos-patch.py" "$SP_DIR"
